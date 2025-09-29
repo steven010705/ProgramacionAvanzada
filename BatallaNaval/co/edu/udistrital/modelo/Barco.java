3f1e77de5e6e23b087c0082c87f9f5ee1b480613
bethods <48637649+steven010705@users.noreply.github.com>
2025-09-29 09:38:33 -0500

    Add javaDocBarco

--- BatallaNaval/co/edu/udistrital/modelo/Barco.java
+++ BatallaNaval/co/edu/udistrital/modelo/Barco.java
@@ -2,13 +2,21 @@ package co.edu.udistrital.modelo;
 
 import java.util.List;
 
+/**
+* Clase Barco
+* @author Steven
+* @version 1.0
+*/
 public class Barco {
     private String tipo;
     private int tamanio;
     private boolean tipoAtaque; // true para ataque, false para defensa
     private int vida;
     private List<int[]> posiciones; // Cada int[] es {x, y}
-
+    
+    /**
+    * Inicializa los atributos
+    */
     public Barco(String tipo, int tamanio, boolean tipoAtaque) {
         this.tipo = tipo;
         this.tamanio = tamanio;
@@ -16,16 +24,27 @@ public class Barco {
         this.vida = tamanio;
     }
 
+    /**
+    * Resta puntos de vida al barco
+    */
     public void recibirAtaque() {
         if (vida > 0) {
             vida--;
         }
     }
 
+    /**
+    * Verifica si un barco está hundido
+    * @return 0
+    */
     public boolean estaHundido() {
         return vida == 0;
     }
 
+    /**
+    * Obtiene los atributos respectivamente
+    * @return atributo correspondiente
+    */
     public int getVida() {
         return vida;
     }
@@ -34,30 +53,33 @@ public class Barco {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public int getTamanio() {
         return tamanio;
-    }
-
-    public void setTamanio(int tamanio) {
-        this.tamanio = tamanio;
-    }
-
-    public boolean isTipoAtaque() {
-        return tipoAtaque;
-    }
-
-    public void setTipoAtaque(boolean tipoAtaque) {
-        this.tipoAtaque = tipoAtaque;
     }
 
     public List<int[]> getPosiciones() {
         return posiciones;
     }
 
+    public boolean isTipoAtaque() {
+        return tipoAtaque;
+    }
+
+    /**
+    * Establece los atributos respectivamente
+    */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setTipoAtaque(boolean tipoAtaque) {
+        this.tipoAtaque = tipoAtaque;
+    }
+    
     public void setPosiciones(List<int[]> posiciones) {
         this.posiciones = posiciones;
     }
