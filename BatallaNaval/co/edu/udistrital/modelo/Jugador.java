cd2c75b78510c7971a44882fca6e94fbfe4ecfae
bethods <48637649+steven010705@users.noreply.github.com>
2025-09-29 14:33:28 -0500

    Add javaDocJugador

--- BatallaNaval/co/edu/udistrital/modelo/Jugador.java
+++ BatallaNaval/co/edu/udistrital/modelo/Jugador.java
@@ -3,17 +3,29 @@ package co.edu.udistrital.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+* Clase Jugador
+* @author Steven
+* @version 1.0
+*/
 public class Jugador {
     private String usuario;
     private String contrasena;
     private List<Barco> barcos;
 
+    /**
+    * Inicializa los atributos
+    * @param usuario primer dato del jugador 
+    * @param contrasena segundo dato del jugador
+    */
     public Jugador(String usuario, String contrasena) {
         this.usuario = usuario;
         this.contrasena = contrasena;
         this.barcos = crearBarcosFijos();
     }
-
+    /**
+    * Añade los distintos barcos del tablero
+    */
     private List<Barco> crearBarcosFijos() {
         List<Barco> barcos = new ArrayList<>();
         barcos.add(new Barco("Portaaviones", 6, true));
@@ -29,36 +41,46 @@ public class Jugador {
         return barcos;
     }
 
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public List<Barco> getBarcos() {
-        return barcos;
-    }
-
-    public void setBarcos(List<Barco> barcos) {
-        this.barcos = barcos;
-    }
-
-    // Puedes agregar aquí métodos para lógica de juego, por ejemplo:
+    /**
+    * Modifica los barcos
+    * @param barco elemento del tablero
+    */
     public void agregarBarco(Barco barco) {
         barcos.add(barco);
     }
 
     public void eliminarBarco(Barco barco) {
         barcos.remove(barco);
+    }
+
+    /**
+    * Obtiene los atributos respectivamente
+    * @return atributo correspondiente
+    */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public List<Barco> getBarcos() {
+        return barcos;
+    }
+
+    /**
+    * Establece los atributos respectivamente
+    */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setBarcos(List<Barco> barcos) {
+        this.barcos = barcos;
     }
 }
