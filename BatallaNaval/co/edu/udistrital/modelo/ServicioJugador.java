fafba22f8056dfcd4d454b881f502ca0dc26fc64
bethods <48637649+steven010705@users.noreply.github.com>
2025-09-29 23:03:38 -0500

    Add javaDocServicioJugador

--- BatallaNaval/co/edu/udistrital/modelo/ServicioJugador.java
+++ BatallaNaval/co/edu/udistrital/modelo/ServicioJugador.java
@@ -3,9 +3,19 @@ package co.edu.udistrital.modelo;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+* Clase ServicioJugador
+* @author Steven
+* @version 1.0
+*/
 public class ServicioJugador {
     private static final String ARCHIVO_USUARIOS = "jugadores.txt";
 
+    /**
+    * Gestiona la creación de usuarios
+    * @param jugador usuario que jugará
+    * @return devuelve si el jugador que se quiere crear existe (true) o no (false)
+    */
     public boolean registrarJugador(Jugador jugador) {
         if (existeUsuario(jugador.getUsuario())) {
             return false;
@@ -14,10 +24,21 @@ public class ServicioJugador {
         return true;
     }
 
+    /**
+    * Gestiona el ingreso de los usuarios creados
+    * @param usuario nombre del jugador
+    * @param contrasena clave del jugador
+    * @return devuelve la verificación de la existencia del jugador
+    */
     public boolean iniciarSesion(String usuario, String contrasena) {
         return validarUsuario(usuario, contrasena);
     }
 
+    /**
+    * Gestiona la persistencia de los usuarios del juego
+    * @param usuario nombre del jugador
+    * @param contrasena clave del jugador
+    */
     private void guardarUsuarioEnArchivo(String usuario, String contrasena) {
         try (FileWriter fw = new FileWriter(ARCHIVO_USUARIOS, true);
              BufferedWriter bw = new BufferedWriter(fw)) {
@@ -28,6 +49,11 @@ public class ServicioJugador {
         }
     }
 
+    /**
+    * Verifica la existencia del usuario en el archivo
+    * @param usuario nombre del jugador
+    * @return devuelve si el usuario existe (true) o no (false)
+    */
     private boolean existeUsuario(String usuario) {
         try (Scanner scanner = new Scanner(new File(ARCHIVO_USUARIOS))) {
             while (scanner.hasNextLine()) {
@@ -43,6 +69,12 @@ public class ServicioJugador {
         return false;
     }
 
+    /**
+    * Valida la existencia del usuario y contraseña ingresados
+    * @param usuario nombre del jugador
+    * @param contrasena clave del jugador
+    * @return devuelve si los datos ingresados existen (true) o no (false)
+    */
     private boolean validarUsuario(String usuario, String contrasena) {
         try (Scanner scanner = new Scanner(new File(ARCHIVO_USUARIOS))) {
             while (scanner.hasNextLine()) {
