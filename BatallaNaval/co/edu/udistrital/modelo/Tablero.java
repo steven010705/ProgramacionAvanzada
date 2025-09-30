4e48d7287cdb31406ca8cb0532d3156e879e23e0
bethods <48637649+steven010705@users.noreply.github.com>
2025-09-29 23:15:47 -0500

    Add javaDocTablero

--- BatallaNaval/co/edu/udistrital/modelo/Tablero.java
+++ BatallaNaval/co/edu/udistrital/modelo/Tablero.java
@@ -1,10 +1,20 @@
 package co.edu.udistrital.modelo;
 
+/**
+* Clase Tablero
+* @author Steven
+* @version 1.0
+*/
 public class Tablero {
     private final int filas;
     private final int columnas;
     private final String[][] tablero;
 
+    /**
+    * Inicializa las casillas
+    * @param filas lineas horizontales del tablero
+    * @param columnas lineas verticales del tablero
+    */
     public Tablero(int filas, int columnas) {
         this.filas = filas;
         this.columnas = columnas;
@@ -12,6 +22,9 @@ public class Tablero {
         inicializarTablero();
     }
 
+    /**
+    * Recorre todas las casillas y las define como agua
+    */
     private void inicializarTablero() {
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
@@ -20,6 +33,14 @@ public class Tablero {
         }
     }
 
+    /**
+    * Gestiona la ubicación de los barcos
+    * @param x coordenada horizontal de la casilla
+    * @param y coordenada vertical de la casilla
+    * @param tamanio numero de casillas por fila y columna
+    * @param horizontal bandera de las filas
+    * @return devuelve que la casilla no tiene un barco (false)
+    */
     public boolean colocarBarco(int x, int y, int tamanio, boolean horizontal) {
         if (horizontal) {
             if (y + tamanio > columnas) return false;
@@ -41,6 +62,12 @@ public class Tablero {
         return true;
     }
 
+    /**
+    * Gestiona los estados de la casilla
+    * @param x coordenada horizontal de la casilla
+    * @param y coordenada vertical de la casilla
+    * @return devuelve si la casilla fue impactada (true) o no (false)
+    */
     public boolean recibirAtaque(int x, int y) {
         if (tablero[x][y].equals("B")) {
             tablero[x][y] = "X"; // Impacto
@@ -52,6 +79,9 @@ public class Tablero {
         return false; // Ya atacado
     }
 
+    /**
+    * Obtiene los atributos respectivamente
+    */
     public String[][] getTablero() {
         return tablero;
     }
