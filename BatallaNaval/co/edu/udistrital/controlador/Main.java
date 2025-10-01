755cb94cb53b5f40d133461226d26e8f47dfcf65
bethods <48637649+steven010705@users.noreply.github.com>
2025-09-30 23:08:19 -0500

    Add javaDocMain

--- BatallaNaval/co/edu/udistrital/controlador/Main.java
+++ BatallaNaval/co/edu/udistrital/controlador/Main.java
@@ -2,9 +2,16 @@ package co.edu.udistrital.controlador;
 
 import co.edu.udistrital.vista.InicioJuego;
 
+/**
+* Clase Main
+* @author Steven
+* @version 1.0
+*/
 public class Main {
+    /** 
+    * Inicia la ventana de inicio de sesión/registro
+    */
     public static void main(String[] args) {
-        // Inicia la ventana de inicio de sesión/registro
         javax.swing.SwingUtilities.invokeLater(() -> new InicioJuego());
     }
 }
